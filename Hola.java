0193e6b0ec2788fefa70c9e8b484a25fc11bee36
Victor Izquierdo <yo@mail.es>
2016-02-15 12:08:34 +0100

    added name property

--- Hola.java
+++ Hola.java
@@ -1,8 +1,10 @@
 public class Hola {
-
+	
+	private static String nombre = "Name";
+	
 	public static void main(String[] zz){
 
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo " + name);
 
 	}
 
